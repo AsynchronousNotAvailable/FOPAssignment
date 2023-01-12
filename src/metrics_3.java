dfa04b508914e6edd8fe72f64ecf9824bd43c9cc
xuejou <22004785@siswa.um.edu.my>
2023-01-12 12:19:29 +0800

    try again

---
+++ src/metrics_3.java
@@ -0,0 +1,5 @@
+public class metrics_3 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class test {
     public static void main(String[] args) {
+        System.out.println("try again");
         System.out.println("kk");
         System.out.println("Hello");
         String s = "2022-3-14";
